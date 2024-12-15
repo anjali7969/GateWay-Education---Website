f9881340a0ba1e704a090c4ca0e24879c88fb656
anjali7969 <123344183+anjali7969@users.noreply.github.com>
2024-12-15 07:34:15 +0545

    deleted backend

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h2>E-Learning</h2>
+            <ul>
+                <li>Home</li>
+                <li>Courses</li>
+                <li>Login</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
